7e9e8344470641033a8f3ddf05f2bb6737817a09
Tim Seryakov <t.seryakov@gmail.com>
2020-10-02 17:01:42 +0300

    task-03a

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-import s from './Header.module.css'
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 
 export const Header = () => {
   return (
 
-      <header className={s.header}>
+      <header className="flex justify-center h-32">
         <ul>
-          <li><NavLink to="/chat"><b>Chat</b> (Task 01) </NavLink></li>
-          <li><NavLink to="/todo"><b>Todo</b> (Task 02) </NavLink></li>
-          <li><NavLink to="/hello"><b>Hello</b> (Task 03) </NavLink></li>
+          <li className="inline-block mt-10 mx-3 p-3 rounded-md bg-green-200 text-green-700 hover:bg-red-200"><NavLink to="/chat"><b>Chat</b> (Task 01) </NavLink></li>
+          <li className="inline-block mt-10 mx-3 p-3 rounded-md bg-green-200 text-green-700 hover:bg-red-200"><NavLink to="/todo"><b>Todo</b> (Task 02) </NavLink></li>
+          <li className="inline-block mt-10 mx-3 p-3 rounded-md bg-green-200 text-green-700 hover:bg-red-200"><NavLink to="/hello"><b>Hello</b> (Task 03) </NavLink></li>
         </ul>
       </header>
 
