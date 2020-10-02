a3622994130ace5caff8e89d5062e63639825827
Tim Seryakov <t.seryakov@gmail.com>
2020-10-02 16:47:29 +0300

    task-03

--- src/components/Hello/Hello.tsx
+++ src/components/Hello/Hello.tsx
@@ -1,19 +1,62 @@
-import React from "react";
-import s from './Hello.module.css'
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
+import {v1} from "uuid";
 
+type VisitorType = {
+  id: string
+  name: string
+}
 
 export const Hello = () => {
-  return (
-      <section className={s.hello}>
 
+  const [name, setName] = useState<string>("")
+  const [visitors, setVisitors] = useState<Array<VisitorType>>([])
+
+  const onChangeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.currentTarget.value)
+  }
+  const onKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onGreetingsButtonClick()
+    }
+  }
+
+  const onGreetingsButtonClick = () => {
+    if (name !== "") {
+      alert(`Hello ${name}`)
+      addName(name)
+      setName("")
+    }
+  }
+
+
+  const addName = (name: string) => {
+      const newVisitor: VisitorType = { id: v1(), name: name }
+      setVisitors([newVisitor, ...visitors])
+  }
+
+  return (
+      <section className="pt-64">
+
+        <div className="flex pb-3 py-4 px-4 w-64 mx-auto">
+          <input
+              className="flex-grow border-gray-500 px-3 py-1 mr-2 border bg-gray-300 rounded-md text-gray-900 focus:outline-none focus:shadow-outline"
+              placeholder="Enter your name..."
+
+              value={name}
+              onChange={onChangeTitle}
+              onKeyPress={onKeyPress}
+          />
+          <button
+              className="bg-blue-400 text-white px-4 py-2 rounded-md focus:outline-none focus:shadow-outline"
+              onClick={onGreetingsButtonClick}>
+            Post
+          </button>
+        </div>
+
+        <div className="text-center">
+           <span className="text-gray-600 font-semibold">Visitors: {visitors.length}</span>
+        </div>
       </section>
   )
 }
 
-/*
-- добавьте в проект с домашками компоненту с инпутом и кнопкой
-- сделайте инпут контролируемым: (useState, value, onChange)
-- сделайте функцию, которая будет здороваться с именем, введённым в инпут (брать value из useState и выводить в алерт) и повесьте её на <button> и добавьте в функцию для onKeyPress инпута
-- выносите стрелочные функции из jsx - это нужно для оптимизации и удобства чтения
-- улучшите функцию: если имя не пустое, то только тогда показывать алерт и добавлять в массив объект с именем и ид используя uuid
-- отобразите количество объектов (имён) спане*/
