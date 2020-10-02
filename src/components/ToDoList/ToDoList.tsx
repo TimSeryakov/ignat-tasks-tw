492a7372c4760c8b799d29ba41447cbf53c0e94b
Tim Seryakov <t.seryakov@gmail.com>
2020-10-02 22:06:50 +0300

    task-03 styles

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import s from './ToDoList.module.css'
 import {FilterPriorityValuesType, TaskType} from "../ToDoBoard/ToDoBoard";
 
 type PropsType = {
@@ -18,9 +17,8 @@ export function ToDoList(props: PropsType) {
     }
 
     return (
-        <li key={t.id} className="bg-gray-800 my-1 rounded-md">
-          <b>{t.name}</b>
-          <span> [{t.priority}]</span>
+        <li key={t.id} className="py-2 px-3 my-3 border border-gray-600 rounded-md flex justify-between items-center">
+          <span><b>{t.name}</b> [{t.priority}]</span>
           <button onClick={removeTasks}><span role="img" aria-label="delete">❌</span></button>
         </li>
     )
@@ -40,18 +38,18 @@ export function ToDoList(props: PropsType) {
   }
 
   return (
-      <section className={s.todolist}>
-        <div className={s["todolist__inner"]}>
-          <h3 className="text-2xl">{props.name}</h3>
-          <ul>
+      <section className="w-full flex justify-center text-gray-100">
+        <div className="border bg-gray-700 inline-block p-6 rounded-lg text-left">
+          <h3 className="text-2xl pb-4">{props.name}</h3>
+          <ul className="list-none">
             {renderTasksList}
           </ul>
           <div>
-            <h4 className="text-xl">Priority filter:</h4>
-            <button onClick={onSetAllFilterClick} className="bg-gray-900 rounded-md p-2">All</button>
-            <button onClick={onSetHeightFilterClick} className="bg-gray-900 rounded-md p-2">Height</button>
-            <button onClick={onSetMiddleFilterClick} className="bg-gray-900 rounded-md p-2">Middle</button>
-            <button onClick={onSetLowFilterClick} className="bg-gray-900 rounded-md p-2">Low</button>
+            <h4 className="text-xl pb-2 pt-4">Priority filter:</h4>
+            <button onClick={onSetAllFilterClick} className="m-1 p-1 bg-gray-800 text-white rounded-md p-2">All</button>
+            <button onClick={onSetHeightFilterClick} className="m-1 p-1 bg-red-600 text-white rounded-md p-2">Height</button>
+            <button onClick={onSetMiddleFilterClick} className="m-1 p-1 bg-blue-600 text-white rounded-md p-2">Middle</button>
+            <button onClick={onSetLowFilterClick} className="m-1 p-1 bg-green-600 text-white rounded-md p-2">Low</button>
           </div>
         </div>
       </section>
