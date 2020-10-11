b24cebfe04b951baf6266725b19948a6ae27476c
Tim Seryakov <t.seryakov@gmail.com>
2020-10-11 19:02:49 +0300

    task-4 purge classes bugfix

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,19 @@ module.exports = {
     // Options passed to PurgeCSS
     options: {
       // Whitelist specific selectors by name
-      // whitelist: [],
+      whitelist: [
+        "h-8", "h-10", "h-12", "px-10", "px-5", "pl-1",
+        "bg-teal-200", "hover:bg-teal-300", "active:bg-teal-400",
+        "bg-green-200", "hover:bg-green-300", "active:bg-green-400",
+        "bg-red-200", "hover:bg-red-300", "active:bg-red-400",
+        "bg-blue-200", "hover:bg-blue-300", "active:bg-blue-400",
+        "bg-pink-200", "hover:bg-pink-300", "active:bg-pink-400",
+        "bg-purple-200", "hover:bg-purple-300", "active:bg-purple-400",
+        "bg-orange-200", "hover:bg-orange-300", "active:bg-orange-400",
+        "bg-yellow-200", "hover:bg-yellow-300", "active:bg-yellow-400",
+      ],
     },
   },
 }
+
+
