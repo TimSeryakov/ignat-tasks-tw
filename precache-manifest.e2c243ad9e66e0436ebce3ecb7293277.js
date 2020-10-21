f2beeb413977470cf78303d2d44b1a4ca79507bb
Tim Seryakov <t.seryakov@gmail.com>
2020-10-21 13:07:49 +0300

    Updates

---
+++ precache-manifest.e2c243ad9e66e0436ebce3ecb7293277.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1117fb9d9bed965136f14a0a8106616",
+    "url": "/ignat-tasks-tw/index.html"
+  },
+  {
+    "revision": "1bfcdbf47e1aafd16f73",
+    "url": "/ignat-tasks-tw/static/css/main.9fd3782b.chunk.css"
+  },
+  {
+    "revision": "e19c11dd77c74d127862",
+    "url": "/ignat-tasks-tw/static/js/2.31ff6adc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ignat-tasks-tw/static/js/2.31ff6adc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bfcdbf47e1aafd16f73",
+    "url": "/ignat-tasks-tw/static/js/main.c42fa95c.chunk.js"
+  },
+  {
+    "revision": "3ae68cb724b9993d6f93",
+    "url": "/ignat-tasks-tw/static/js/runtime-main.e2bec51a.js"
+  },
+  {
+    "revision": "0e1c42b1a6180c7dc33ee6b5c4916061",
+    "url": "/ignat-tasks-tw/static/media/fuji.0e1c42b1.png"
+  },
+  {
+    "revision": "de4622b836e54d68e9c5a1da74149e50",
+    "url": "/ignat-tasks-tw/static/media/koifish.de4622b8.png"
+  },
+  {
+    "revision": "6c09dc744af620371857e2e815371bc2",
+    "url": "/ignat-tasks-tw/static/media/lionstatue.6c09dc74.png"
+  },
+  {
+    "revision": "0257e4c846d44b78d95dfd12f33b521b",
+    "url": "/ignat-tasks-tw/static/media/luckycat.0257e4c8.png"
+  }
+]);
